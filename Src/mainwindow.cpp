0cd2b05951e2e62d6b6c8350a7ccb6001b830fd2
Administrator <admin@example.com>
2020-07-12 10:00:55 +0800

    修改相机显示方式

--- Src/mainwindow.cpp
+++ Src/mainwindow.cpp
@@ -181,141 +181,159 @@ bool MainWindow::ShowImage(uint8_t* pRgbFrameBuf, int pRgbFrameBufSize, int nWid
 	{
 		image = QImage(pRgbFrameBuf, nWidth, nHeight, QImage::Format_RGB888);
 	}
+
+	CEnumNode nodeEnum(m_pCamera, "TriggerSource");
+	CString strValue;
+	nodeEnum.getValueSymbol(strValue);
+	if (strValue == "Line1") {
+		ui->label_7->setText("Line1");
+	}
+	if (strValue == "Software")
+	{
+		ui->label_7->setText("Software");
+	}
 	
 	//显示整幅图片
+	//QImage imageScale = image.scaled(QSize(ui->label->width(), ui->label->height()));
 	QPixmap pixmap = QPixmap::fromImage(image);
-	int w = ui->label->width();
-	int h = ui->label->height();	
-	ui->label->setPixmap(pixmap.scaled(w,h,Qt::KeepAspectRatio));
-	ui->lcdNumber_2->display(++total_number);
-	Config().Set("Count", "Count", total_number);
-	//每增加一百个就写入一次
-	if (0 == total_number % 100) {
-		//参考连接https://www.cnblogs.com/findumars/p/7252854.html
-		ofstream outFile;
-		outFile.open("count.csv", ios::app); // 打开模式可省略  
-		outFile << get_datetime() << ',' << "总数" << ',' << total_number << ',' << "正确总数" << ',' << sum_of_correct << ',' << "错误总数" << ',' << sum_of_wrong << endl;
-		outFile.close();
-	}
+	ui->label->setPixmap(pixmap);
+	free(pRgbFrameBuf);
 
-	//label2显示裁剪之后的照片
-	Mat img = QImage2cvMat(image);
-	Pic_to_Save = img;
-	if (true == Mode_of_trig_soft) {
-		imwrite("Train/image/Pic.bmp", Pic_to_Save);
-		ui->label_3->setText("保存图像成功");
-		//QMessageBox::information(this, "保存图片成功", "保存图像成功");
-		Mode_of_trig_soft = false;
-	}
-	//裁剪并显示
-	img = img(rect_of_image); 
-	cv::Mat out;
-	cv::Mat in[] = { img, img, img };
-	cv::merge(in, 3, out);
-	//ui->label_2->setPixmap(QPixmap::fromImage(cvMat2QImage(out)));
-	//识别裁剪后的图片
-	try
-	{
-		startTime = clock();
-		if (!bookdetection(out))//识别判断
-		{
-			ui->lcdNumber_3->display(++sum_of_wrong);
-			//Config().Set("Count", "sum_of_wrong", sum_of_wrong);
-			//Beep(1000, 1000);
-			//cout << "不合格" << endl << endl;
-			//emit StartThread();
-			//弹窗报警,2秒自动关闭
-			//alertWindow = new AlertWindow;
-			//alertWindow->startTimer();
-			//alertWindow->exec();
-			//output file
-			//imwrite(wrong_filename, src_mat);
-			//run_database(current_time, "不合格");
-			//unsigned char uc[] = { 0x7e,0x01,0x55,0x55,0x55,0x55 };
-			//int count = 0;
-			//while (revFlag != true) {
-			//	revFlag = mycserialport.WriteData(uc, 6);
-			//	Sleep(50);
-			//	count++;
-			//	if (count >= 3) {
-			//		//cout << "未收到下位机确认信息!" << endl;
-			//		//连续发三次，三次握手,返回动作执行成功
-			//		count = 0;
-			//		break;
-			//	}
-			//}
-			//revFlag = false;
-		}
-		else {
-			ui->lcdNumber->display(++sum_of_correct);
-			Config().Set("Count", "sum_of_correct", sum_of_correct);
-		}
-		endTime = clock();
-		string s = get_datetime() + "运行时间: " + to_string((double)(endTime - startTime) / CLOCKS_PER_SEC) + "s";
-		QString st = QString::fromStdString(s);
-		ui->label_7->setText(st);
-	}
-	catch (const std::exception& e)
-	{
-		QMessageBox::information(NULL, "识别部分出错", e.what());
-		return true;
-	}
+	//QPixmap pixmap = QPixmap::fromImage(image);
+	//int w = ui->label->width();
+	//int h = ui->label->height();
+	//ui->label->setPixmap(pixmap.scaled(w, h, Qt::KeepAspectRatio));
 
-	//else if (true == Mode_of_trig_soft) {
-	//	try
-	//	{
-	//		Pic_to_Save = QImage2cvMat(image);
-	//		ui->label_2->setPixmap(QPixmap::fromImage(cvMat2QImage(Pic_to_Save)));
-	//		////imshow("test", img);
-	//		//bool ret = imwrite("Train/image/Pic.bmp", img);
-	//		////cv::Mat out;
-	//		////cv::Mat in[] = { img, img, img };
-	//		////cv::merge(in, 3, out);
-	//		////ui->label_2->setPixmap(QPixmap::fromImage(cvMat2QImage(out)));
-	//		//////QMessageBox::information(NULL, "img channels", QString::number(out.channels()));
-	//		////if (ret) {
-	//		////	//QMessageBox::information(NULL, "保存图片", "保存图片成功！");
-	//		////	ui->label_3->setText("保存图片成功");
-	//		////	Mode_of_trig_soft = false;
-	//		////}
-	//		////else
-	//		////{
-	//		////	QMessageBox::warning(NULL, "保存图片失败！", "保存图片失败！");
-	//		////}
-	//		//Mat i = imread("Train/image/Pic.bmp");
-	//		//ui->label_2->setPixmap(QPixmap::fromImage(cvMat2QImage(i)));
-	//	}
-	//	catch (const std::exception& e)
-	//	{
-	//		QMessageBox::information(NULL, "识别部分出错", e.what());
-	//		return true;
-	//	}
+	//ui->lcdNumber_2->display(++total_number);
+	//Config().Set("Count", "Count", total_number);
+	//
+	////计数功能 每增加一百个就写入一次
+	//if (0 == total_number % 100) {
+	//	//参考连接https://www.cnblogs.com/findumars/p/7252854.html
+	//	ofstream outFile;
+	//	outFile.open("count.csv", ios::app); // 打开模式可省略，此处用的是添加模式
+	//	outFile << get_datetime() << ',' << "总数" << ',' << total_number << ',' << "正确总数" << ',' << sum_of_correct << ',' << "错误总数" << ',' << sum_of_wrong << endl;
+	//	outFile.close();
+	//}
+
+	////label2显示裁剪之后的照片
+	//Mat img = QImage2cvMat(image);
+	//Pic_to_Save = img;
+	//if (true == Mode_of_trig_soft) {
+	//	imwrite("Train/image/Pic.bmp", Pic_to_Save);
+	//	ui->label_3->setText("保存图像成功");
+	//	//QMessageBox::information(this, "保存图片成功", "保存图像成功");
 	//	Mode_of_trig_soft = false;
 	//}
-	//else
+	////裁剪并显示
+	//img = img(rect_of_image); 
+	//cv::Mat out;
+	//cv::Mat in[] = { img, img, img };
+	//cv::merge(in, 3, out);
+	////ui->label_2->setPixmap(QPixmap::fromImage(cvMat2QImage(out)));
+	////识别裁剪后的图片
+	//try
 	//{
-	//	//单击按钮保存图片
-	//	if (true == Mode_of_trig_soft) {
-	//		try
-	//		{
-	//			Mat img = QImage2cvMat(image);
-	//			Mode_of_trig_soft = false;
-	//			cv::Mat out;
-	//			cv::Mat in[] = { img, img, img };
-	//			cv::merge(in, 3, out);
-	//			//ui->label_2->setPixmap(QPixmap::fromImage(cvMat2QImage(out)));
-	//			//QMessageBox::information(NULL, "img channels", QString::number(out.channels()));
-	//			imwrite("Train/image/Pic.bmp", img);
-	//			QMessageBox::information(NULL, "保存图片", "保存图片成功！");
-	//		}
-	//		catch (const std::exception& e)
-	//		{
-	//			QMessageBox::information(NULL, "识别部分出错", e.what());
-	//			return true;
-	//		}
-	//		Mode_of_trig_soft = false;
+	//	startTime = clock();
+	//	if (!bookdetection(out))//识别判断
+	//	{
+	//		ui->lcdNumber_3->display(++sum_of_wrong);
+	//		//Config().Set("Count", "sum_of_wrong", sum_of_wrong);
+	//		//Beep(1000, 1000);
+	//		//cout << "不合格" << endl << endl;
+	//		//emit StartThread();
+	//		//弹窗报警,2秒自动关闭
+	//		//alertWindow = new AlertWindow;
+	//		//alertWindow->startTimer();
+	//		//alertWindow->exec();
+	//		//output file
+	//		//imwrite(wrong_filename, src_mat);
+	//		//run_database(current_time, "不合格");
+	//		//unsigned char uc[] = { 0x7e,0x01,0x55,0x55,0x55,0x55 };
+	//		//int count = 0;
+	//		//while (revFlag != true) {
+	//		//	revFlag = mycserialport.WriteData(uc, 6);
+	//		//	Sleep(50);
+	//		//	count++;
+	//		//	if (count >= 3) {
+	//		//		//cout << "未收到下位机确认信息!" << endl;
+	//		//		//连续发三次，三次握手,返回动作执行成功
+	//		//		count = 0;
+	//		//		break;
+	//		//	}
+	//		//}
+	//		//revFlag = false;
 	//	}
+	//	else {
+	//		ui->lcdNumber->display(++sum_of_correct);
+	//		Config().Set("Count", "sum_of_correct", sum_of_correct);
+	//	}
+	//	endTime = clock();
+	//	string s = get_datetime() + "运行时间: " + to_string((double)(endTime - startTime) / CLOCKS_PER_SEC) + "s";
+	//	QString st = QString::fromStdString(s);
+	//	ui->label_7->setText(st);
 	//}
+	//catch (const std::exception& e)
+	//{
+	//	QMessageBox::information(NULL, "识别部分出错", e.what());
+	//	return true;
+	//}
+
+	////else if (true == Mode_of_trig_soft) {
+	////	try
+	////	{
+	////		Pic_to_Save = QImage2cvMat(image);
+	////		ui->label_2->setPixmap(QPixmap::fromImage(cvMat2QImage(Pic_to_Save)));
+	////		////imshow("test", img);
+	////		//bool ret = imwrite("Train/image/Pic.bmp", img);
+	////		////cv::Mat out;
+	////		////cv::Mat in[] = { img, img, img };
+	////		////cv::merge(in, 3, out);
+	////		////ui->label_2->setPixmap(QPixmap::fromImage(cvMat2QImage(out)));
+	////		//////QMessageBox::information(NULL, "img channels", QString::number(out.channels()));
+	////		////if (ret) {
+	////		////	//QMessageBox::information(NULL, "保存图片", "保存图片成功！");
+	////		////	ui->label_3->setText("保存图片成功");
+	////		////	Mode_of_trig_soft = false;
+	////		////}
+	////		////else
+	////		////{
+	////		////	QMessageBox::warning(NULL, "保存图片失败！", "保存图片失败！");
+	////		////}
+	////		//Mat i = imread("Train/image/Pic.bmp");
+	////		//ui->label_2->setPixmap(QPixmap::fromImage(cvMat2QImage(i)));
+	////	}
+	////	catch (const std::exception& e)
+	////	{
+	////		QMessageBox::information(NULL, "识别部分出错", e.what());
+	////		return true;
+	////	}
+	////	Mode_of_trig_soft = false;
+	////}
+	////else
+	////{
+	////	//单击按钮保存图片
+	////	if (true == Mode_of_trig_soft) {
+	////		try
+	////		{
+	////			Mat img = QImage2cvMat(image);
+	////			Mode_of_trig_soft = false;
+	////			cv::Mat out;
+	////			cv::Mat in[] = { img, img, img };
+	////			cv::merge(in, 3, out);
+	////			//ui->label_2->setPixmap(QPixmap::fromImage(cvMat2QImage(out)));
+	////			//QMessageBox::information(NULL, "img channels", QString::number(out.channels()));
+	////			imwrite("Train/image/Pic.bmp", img);
+	////			QMessageBox::information(NULL, "保存图片", "保存图片成功！");
+	////		}
+	////		catch (const std::exception& e)
+	////		{
+	////			QMessageBox::information(NULL, "识别部分出错", e.what());
+	////			return true;
+	////		}
+	////		Mode_of_trig_soft = false;
+	////	}
+	////}
 	return true;
 }
 
@@ -1527,45 +1545,45 @@ void MainWindow::on_pushButton_clicked()
 
 	//-------------------------------------------------------------------------------
 	//不需要摄像头，本地文件测试函数用
-	testRun();
+	//testRun();
 
 	//----------------------------上下互斥-------------------------------------------
 	//连接摄像头实时监测用
 	//打开相机
-	//ICameraPtr cameraSptr;
-	////发现设备
-	//CSystem &systemObj = CSystem::getInstance();
-	//TVector<ICameraPtr> vCameraPtrList;
-	//bool bRet = systemObj.discovery(vCameraPtrList);
+	ICameraPtr cameraSptr;
+	//发现设备
+	CSystem &systemObj = CSystem::getInstance();
+	TVector<ICameraPtr> vCameraPtrList;
+	bool bRet = systemObj.discovery(vCameraPtrList);
 
-	//if (!bRet || 0 == vCameraPtrList.size())
-	//{
-	//	QMessageBox::warning(NULL, "warning", "发现设备失败\n", QMessageBox::Yes | QMessageBox::No, QMessageBox::Yes);
-	//	ui->pushButton->setEnabled(true);
-	//	ui->pushButton_2->setEnabled(false);
-	//	ui->pushButton_3->setEnabled(false);
-	//	ui->pushButton_4->setEnabled(false);
-	//	return;
-	//}
-	//else {
-	//	try {
-	//		CameraCheck();
-	//		bool camera_open = CameraOpen();
-	//		CameraStart();
-	//		ui->label_3->setText("相机连接成功！");
-	//		//SetExposeTime(10000);
-	//		//SetAdjustPlus(5);
-	//		CameraChangeTrig(trigLine);
-	//		//CameraChangeTrig(trigContinous);
-	//		//ui->label_2->setEnabled(false);
-	//		//ui->label_2->setVisible(false);
-	//		ui->pushButton_3->setEnabled(false);
-	//		ui->pushButton_4->setEnabled(false);
-	//	}
-	//	catch (Exception e) {
-	//		QMessageBox::warning(NULL, "warning in open camera", e.what(), QMessageBox::Yes | QMessageBox::No, QMessageBox::Yes);
-	//	}
-	//}
+	if (!bRet || 0 == vCameraPtrList.size())
+	{
+		QMessageBox::warning(NULL, "warning", "发现设备失败\n", QMessageBox::Yes | QMessageBox::No, QMessageBox::Yes);
+		ui->pushButton->setEnabled(true);
+		ui->pushButton_2->setEnabled(false);
+		ui->pushButton_3->setEnabled(false);
+		ui->pushButton_4->setEnabled(false);
+		return;
+	}
+	else {
+		try {
+			CameraCheck();
+			bool camera_open = CameraOpen();
+			CameraStart();
+			ui->label_3->setText("相机连接成功！");
+			//SetExposeTime(10000);
+			//SetAdjustPlus(5);
+			//CameraChangeTrig(trigLine);
+			CameraChangeTrig(trigContinous);
+			//ui->label_2->setEnabled(false);
+			//ui->label_2->setVisible(false);
+			ui->pushButton_3->setEnabled(false);
+			ui->pushButton_4->setEnabled(false);
+		}
+		catch (Exception e) {
+			QMessageBox::warning(NULL, "warning in open camera", e.what(), QMessageBox::Yes | QMessageBox::No, QMessageBox::Yes);
+		}
+	}
 
 	//-------------------------------------------------------------------------------
 }
